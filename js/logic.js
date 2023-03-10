ac5a2d33c7615782db2f45f405ac8e37f8e37a21
ayirac <garre058@cougars.csusm.edu>
2023-03-09 23:49:02 -0800

    swapped from tkinter to html/javascript due to frame buffer

---
+++ js/logic.js
@@ -0,0 +1,23 @@
+// Movement Keybinds
+$(document).keydown(function(event) {
+    switch(event.which){
+        case 87:
+            alert('W key pressed!');
+            break;
+        case 65:
+            alert('A key pressed!');
+            break;
+        case 83:
+            alert('S key pressed!');
+            break;
+        case 68:
+            alert('S key pressed!');
+            break;
+        case 81:
+            alert('Q key pressed!');
+            break;
+        case 69:
+            alert('E key pressed!');
+            break;
+    }
+    });
