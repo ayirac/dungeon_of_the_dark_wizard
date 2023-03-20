fb58516a63c61ce0d9fe8f70021ea7f7d74120ce
ayirac <ayirac@cougars.csusm.edu>
2023-03-19 21:48:35 -0700

    part1 of finished keybind logic

--- js/logic.js
+++ js/logic.js
@@ -1,17 +1,29 @@
+/// Todo, need to make JSON struct holding imgs for each direction & Pull from it
+cur_pos = '(0,0)'
+var map;
+$.getJSON("data.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
+    map = json
+});
 // Movement Keybinds
 $(document).keydown(function(event) {
+    direction = "n/a";
     switch(event.which){
         case 87:
             alert('W key pressed!');
+            direction = "north"
             break;
         case 65:
             alert('A key pressed!');
+            direction = "west"
             break;
         case 83:
             alert('S key pressed!');
+            direction = "south"
             break;
         case 68:
             alert('S key pressed!');
+            direction = "east"
             break;
         case 81:
             alert('Q key pressed!');
@@ -20,4 +32,14 @@ $(document).keydown(function(event) {
             alert('E key pressed!');
             break;
     }
-    });
+    if (map[cur_pos][direction]['type'] == 'wall')
+        alert("WALL HIT");
+        // DONT MOVE
+    else if (map[cur_pos][direction]['type'] == 'air')
+        alert("AIR!")
+        // MOVE!
+        // (x,y), need to parse this string into int
+        pos = cur_pos.replace(/[()]/g, '');
+        x = pos.replace(//g, '');
+        alert(x) // cont here
+});
